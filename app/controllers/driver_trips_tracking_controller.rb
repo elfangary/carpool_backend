0b0d1aaed29a0539c269739ec4b0555c863c0168
Dina Waleed <Dina@Dinas-MacBook-Pro-5.local>
2018-01-07 14:14:35 +0200

    Merge branches

--- app/controllers/driver_trips_tracking_controller.rb
+++ app/controllers/driver_trips_tracking_controller.rb
@@ -1,5 +1,5 @@
 class DriverTripsTrackingController < ApplicationController
-  
+
   def index
     trips = current_user.driver_trips_tracking(params[:time])
     render json: trips.to_json(:include => {:stop_points => {:include => [:location, :hh_stop_points => {:include => :hh}]}, :driver => {}}), status: :ok
