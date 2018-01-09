64ab23a49a5c92e7aff772c98556a32aa575cba3
alaa <alaatohamy4@gmail.com>
2018-01-09 20:22:23 +0200

    add notification in change_hh controller

--- app/controllers/change_hh_stop_point_status_controller.rb
+++ app/controllers/change_hh_stop_point_status_controller.rb
@@ -1,12 +1,18 @@
 class ChangeHhStopPointStatusController < ApplicationController
     before_action :set_hh_stop_point, only: [:update]
+    after_action :set_read_attribute
 
     def update
         hh_stop_point = @hh_stop_point.accept_or_reject_hhStopPoint(params[:confirm])
         render json: hh_stop_point, status: :ok
     end
 
+    def set_read_attribute
+      @notifications = current_user.notifications.update_all(read: true)
+    end
+
+    private
     def set_hh_stop_point
-        @hh_stop_point = HhStopPoint.find params[:id]
+      @hh_stop_point = HhStopPoint.find params[:id]
     end
 end
