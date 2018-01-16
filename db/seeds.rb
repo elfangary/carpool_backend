22a5f61425e464bf9fe1da11bf20cb25fa77faae
Dina Waleed <dina.hamamo@gmail.com>
2018-01-16 20:34:23 +0200

    Lastest conflicts

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,40 +6,34 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-users = User.create([
+users = User.create!([
  {first_name: 'Alaa', last_name: 'Tohamy', email: 'alaatohamy4@gmail.com', phone: '0121111134', password: 'password', password_confirmation: 'password', gender: 'female'},
 
  {first_name: 'Dina', last_name: 'hamamo', email: 'dinamhamamo@gmail.com', phone: '0121222134', password: 'password', password_confirmation: 'password', gender: 'female'},
  {first_name: 'ahmed', last_name: 'fangary', email: 'ahmedfangary@gmail.com', phone: '0121777134', password: 'password', password_confirmation: 'password', gender: 'male'}
   ])
 
-cars = Car.create([
+cars = Car.create!([
   {user_id: 2, model: 'Toyota Yaris', color: 'black', number: '123456'},
   {user_id: 2, model: 'opel astra', color: 'red', number: '123466'},
   {user_id: 1, model: 'Hyundai Accent.', color: 'blue', number: '132'}
   ])
 
-locations = Location.create([
+locations = Location.create!([
   {name: 'carrefour maadi'},
   {name: 'Naser city'},
   {name: '6 october'}
   ])
 
-trips = Trip.create([
-  {car_id: 1, driver_id: 1, day: "2018-01-03", all_seats: 4},
-  {car_id: 1, driver_id: 1, day: "2018-01-04", all_seats: 4}
+trips = Trip.create!([
+  {car_id: 1, driver_id: 1, day: "2018-01-20", all_seats: 4},
+  {car_id: 1, driver_id: 1, day: "2018-01-21", all_seats: 4}
   ])
 
-
-stop_points = StopPoint.create([
+stop_points = StopPoint.create!([
   {start_time: '02:00', end_time: '02:30', location_id: 1, trip_id:1},
   {start_time: '04:00', end_time: '04:30', location_id: 2, trip_id:1},
   {start_time: '02:00', end_time: '02:30', location_id: 1, trip_id:2}
-  ])
-
-hh_stop_points = HhStopPoint.create([
-  {hh_id: 3, booked_seats: 1, stop_point_id: 1},
-  {hh_id: 2, booked_seats: 2, stop_point_id: 2}
   ])
 
 hh_stop_points = HhStopPoint.create!([
